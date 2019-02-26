048bdd9491deafd3eb2a5353493093a51c888bf9
Alex Suevalov <alex.suevalov@contentful.com>
2019-02-26 12:41:20 +0100

    Used simple imports from Forma

--- packages/contentful-extension-scripts/template/fields/src/index.js
+++ packages/contentful-extension-scripts/template/fields/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import TextInput from '@contentful/forma-36-react-components/dist/components/TextInput/TextInput';
+import { TextInput } from '@contentful/forma-36-react-components';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import { init } from 'contentful-ui-extensions-sdk';
 
