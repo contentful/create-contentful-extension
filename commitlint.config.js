c1343033867a62d45b83b94470bff8c8abbfb315
Alex Suevalov <alex.suevalov@contentful.com>
2019-03-01 12:11:19 +0100

    chore: change commitlint settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,6 @@ module.exports = {
   rules: {
     'scope-case': [0, 'always', ['lower-case', 'pascal-case', 'camel-case']],
     'subject-case': [0, 'always', ['lower-case', 'sentence-case']],
+    'header-max-length': [0, 'always', 100],
   },
 };
