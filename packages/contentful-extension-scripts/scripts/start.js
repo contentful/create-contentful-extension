2e063dc22f7afa5bd01199c624f7129db95a7b91
Connor Bär <connor.baer@sumup.com>
2019-03-01 23:52:09 +0100

    feat: Enable https in development

--- packages/contentful-extension-scripts/scripts/start.js
+++ packages/contentful-extension-scripts/scripts/start.js
@@ -23,6 +23,7 @@ const options = {
   sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: '', // A hostname for hot module reload, default to ''
   detailedReport: false, // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
+  https: true, // This flag generates a self-signed certificate, you might have to configure your browser to allow self-signed certificates for localhost.
 };
 
 const bundler = new Bundler(entry, options);
