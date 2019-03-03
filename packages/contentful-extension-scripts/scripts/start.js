002a1eaa96e8383bbb8700095331bba16c3b0035
Connor Bär <connor.baer@sumup.com>
2019-03-03 18:36:36 +0100

    fix: Hard-code the websocket port

--- packages/contentful-extension-scripts/scripts/start.js
+++ packages/contentful-extension-scripts/scripts/start.js
@@ -19,7 +19,7 @@ const options = {
   scopeHoist: false, // Turn on experimental scope hoisting/tree shaking flag, for smaller production bundles
   logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
   hmr: true, // Enable or disable HMR while watching
-  hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
+  hmrPort: 54321, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
   sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: '', // A hostname for hot module reload, default to ''
   detailedReport: false, // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
