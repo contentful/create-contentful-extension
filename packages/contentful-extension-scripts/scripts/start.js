46fce3ef6574f56aeb248117051a98d03508951e
Connor Bär <connor-baer@users.noreply.github.com>
2019-03-04 16:10:34 +0100

    feat: Enable https in development (#4)

--- packages/contentful-extension-scripts/scripts/start.js
+++ packages/contentful-extension-scripts/scripts/start.js
@@ -6,6 +6,7 @@ const Bundler = require('parcel-bundler');
 const paths = require('./utils/paths');
 
 const entry = paths.src + '/index.html';
+const https = process.env.HTTPS === 'true';
 
 // Bundler options
 const options = {
@@ -19,17 +20,18 @@ const options = {
   scopeHoist: false, // Turn on experimental scope hoisting/tree shaking flag, for smaller production bundles
   logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
   hmr: true, // Enable or disable HMR while watching
-  hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
+  hmrPort: 54321, // The port the HMR socket runs on
   sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: '', // A hostname for hot module reload, default to ''
   detailedReport: false, // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
+  https: https, // This flag generates a self-signed certificate, you might have to configure your browser to allow self-signed certificates for localhost
 };
 
 const bundler = new Bundler(entry, options);
 
 const run = async () => {
   const port = process.env.PORT || 1234;
-  await bundler.serve(port);
+  await bundler.serve(port, https);
 };
 
 run();
