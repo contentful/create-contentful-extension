513ccb591f1ddbe1007f03dad91e8d30e49e2a19
Alex Suevalov <alex.suevalov@contentful.com>
2019-03-22 13:03:31 +0100

    feat: added ability to generate Typescript extension

---
+++ packages/contentful-extension-scripts/template/typescript-fields/src/index.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { TextInput } from '@contentful/forma-36-react-components';
+import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import './index.css';
+
+interface AppProps {
+  sdk: FieldExtensionSDK;
+}
+
+interface AppState {
+  value?: string;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      value: props.sdk.field.getValue(),
+    };
+  }
+
+  detachExternalChangeHandler: Function | null = null;
+
+  componentDidMount() {
+    this.props.sdk.window.startAutoResizer();
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
+      this.onExternalChange
+    );
+  }
+
+  componentWillUnmount() {
+    if (this.detachExternalChangeHandler) {
+      this.detachExternalChangeHandler();
+    }
+  }
+
+  onExternalChange = (value: string) => {
+    this.setState({ value });
+  };
+
+  onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    this.setState({ value });
+    if (value) {
+      this.props.sdk.field.setValue(value);
+    } else {
+      this.props.sdk.field.removeValue();
+    }
+  };
+
+  render = () => {
+    return (
+      <TextInput
+        width="large"
+        type="text"
+        id="my-field"
+        value={this.state.value}
+        onChange={this.onChange}
+      />
+    );
+  };
+}
+
+init(sdk => {
+  render(
+    <App sdk={sdk as FieldExtensionSDK} />,
+    document.getElementById('root')
+  );
+});
